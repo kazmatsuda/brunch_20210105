0d9843500feb185fc1db3fc30bf9e1c03dafd5f3
Miho Horiuchi <paglinawan0520@gmail.com>
2021-01-05 15:08:29 +0900

    修正：初期段階に戻し

--- archive.php
+++ archive.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
@@ -44,4 +44,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
+	<?php get_sidebar(); ?>
 <?php get_footer(); ?>
