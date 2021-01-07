d0a91cdff9488c53f9b787127a6b76063f0dab6d
Miho Horiuchi <paglinawan0520@gmail.com>
2021-01-07 23:12:35 +0900

    修正：他のページが崩れないようにsearchクラスを追加

--- search.php
+++ search.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<section id="primary" class="content-area search">
 		<main id="main" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
