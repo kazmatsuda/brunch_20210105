be06683812a13a149173c392aa2697ee385b6d09
kazmatsudaq <kazmatsuda7006300@gmail.com>
2021-01-11 18:42:15 +0900

    戻し

--- footer.php
+++ footer.php
@@ -18,13 +18,12 @@
 
 			<div class="site-info">
 				<div class="site-copyright">
-					<!-- &copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> -->
-					&copy; <?php printf( wp_kses( __( 'Y' ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				</div><!-- .site-copyright -->
-				<!-- <div class="site-credit"> -->
-				<!-- <?php printf( wp_kses( __( 'Powered by <a href="%1$s">%2$s</a>', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'https://wordpress.org/', 'graphy' ) ), 'WordPress' ); ?> -->
-				<!-- <span class="site-credit-sep"> | </span> -->
-					<!-- <?php printf( wp_kses( __( 'Theme: <a href="%1$s">%2$s</a> by %3$s', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'http://themegraphy.com/wordpress-themes/graphy/', 'graphy' ) ), 'Graphy', 'Themegraphy' ); ?> -->
+				<div class="site-credit">
+					<?php printf( wp_kses( __( 'Powered by <a href="%1$s">%2$s</a>', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'https://wordpress.org/', 'graphy' ) ), 'WordPress' ); ?>
+				<span class="site-credit-sep"> | </span>
+					<?php printf( wp_kses( __( 'Theme: <a href="%1$s">%2$s</a> by %3$s', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'http://themegraphy.com/wordpress-themes/graphy/', 'graphy' ) ), 'Graphy', 'Themegraphy' ); ?>
 				</div><!-- .site-credit -->
 			</div><!-- .site-info -->
 
