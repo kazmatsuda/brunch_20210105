886c3e4c0784874f569e2f09aa2491e8c340ef50
kazmatsudaq <kazmatsuda7006300@gmail.com>
2021-01-11 18:33:17 +0900

    footer 調整

--- footer.php
+++ footer.php
@@ -22,8 +22,8 @@
 				</div><!-- .site-copyright -->
 				<!-- <div class="site-credit"> -->
 				<!-- <?php printf( wp_kses( __( 'Powered by <a href="%1$s">%2$s</a>', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'https://wordpress.org/', 'graphy' ) ), 'WordPress' ); ?> -->
-				<span class="site-credit-sep"> | </span>
-					<?php printf( wp_kses( __( 'Theme: <a href="%1$s">%2$s</a> by %3$s', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'http://themegraphy.com/wordpress-themes/graphy/', 'graphy' ) ), 'Graphy', 'Themegraphy' ); ?>
+				<!-- <span class="site-credit-sep"> | </span> -->
+					<!-- <?php printf( wp_kses( __( 'Theme: <a href="%1$s">%2$s</a> by %3$s', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'http://themegraphy.com/wordpress-themes/graphy/', 'graphy' ) ), 'Graphy', 'Themegraphy' ); ?> -->
 				</div><!-- .site-credit -->
 			</div><!-- .site-info -->
 
