30c8ce27ae9a8d6cb4fde1b6d7194dcdefd2e3b3
kazmatsudaq <kazmatsuda7006300@gmail.com>
2021-01-11 18:28:26 +0900

    no message

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
 					&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				</div><!-- .site-copyright -->
 				<!-- <div class="site-credit"> -->
-					<?php printf( wp_kses( __( 'Powered by <a href="%1$s">%2$s</a>', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'https://wordpress.org/', 'graphy' ) ), 'WordPress' ); ?>
+				<!-- <?php printf( wp_kses( __( 'Powered by <a href="%1$s">%2$s</a>', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'https://wordpress.org/', 'graphy' ) ), 'WordPress' ); ?> -->
 				<span class="site-credit-sep"> | </span>
 					<?php printf( wp_kses( __( 'Theme: <a href="%1$s">%2$s</a> by %3$s', 'graphy' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( __( 'http://themegraphy.com/wordpress-themes/graphy/', 'graphy' ) ), 'Graphy', 'Themegraphy' ); ?>
 				</div><!-- .site-credit -->
